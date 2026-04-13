6ef1b9816a7cf53b1917316b74c78b5b31cfce48
Sezim Orozobekova <orozobekovasezim.kg@gmail.com>
2026-04-14 00:55:46 +0600

    feat(security): add user security configurations

---
+++ src/main/java/kg/alatoo/smarthousebackendsystem/security/CustomUserDetails.java
@@ -0,0 +1,61 @@
+package kg.alatoo.smarthousebackendsystem.security;
+
+import kg.alatoo.smarthousebackendsystem.user.entity.User;
+import kg.alatoo.smarthousebackendsystem.user.entity.UserStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public UUID getId() {
+        return user.getId();
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(user.getRole().getName()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPasswordHash();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return user.getStatus() != UserStatus.PENDING;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return Boolean.TRUE.equals(user.getIsActive());
+    }
+}
