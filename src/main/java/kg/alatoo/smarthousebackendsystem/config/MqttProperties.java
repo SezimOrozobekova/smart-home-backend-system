3c7abb874358dedb4eaf4d19efe60c03143addbe
Sezim Orozobekova <orozobekovasezim.kg@gmail.com>
2026-04-18 01:02:01 +0600

    feat: add mqtt config and properties

--- src/main/java/kg/alatoo/smarthousebackendsystem/config/MqttProperties.java
+++ src/main/java/kg/alatoo/smarthousebackendsystem/config/MqttProperties.java
@@ -3,24 +3,19 @@ package kg.alatoo.smarthousebackendsystem.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
-@Configuration
+@Component
 @ConfigurationProperties(prefix = "mqtt")
 public class MqttProperties {
 
     private boolean enabled;
-    private String host;
-    private int port;
+    private String brokerUrl;
     private String clientId;
     private String username;
     private String password;
-    private boolean cleanSession;
-    private boolean autoReconnect;
-
-    public String getBrokerUrl() {
-        return "tcp://" + host + ":" + port;
-    }
+    private boolean autoReconnect = true;
+    private boolean cleanSession = true;
 }
